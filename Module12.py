87df87d1c1d7f715bfa7b77b6142c1ab926dc001
PepaKen <74868751+PepaKen@users.noreply.github.com>
2020-12-16 19:43:23 +0300

    Module 12.7 Results

---
+++ Module12.py
@@ -0,0 +1,27 @@
+D = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input("Введите сумму вклада:"))
+depositTKB = float(money / 100 * D['ТКБ']) + money
+depositSKB = float(money / 100 * D['СКБ']) + money
+depositVTB = float(money / 100 * D['ВТБ']) + money
+depositSBER = float(money / 100 * D['СБЕР']) + money
+deposits = [depositTKB, depositSKB, depositVTB, depositSBER]
+print(deposits)
+print("Максимальная сумма, которую вы можете заработать -", max(deposits))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
